75f036aa31b8bfc19e770367afaf049c13988050
Brad Goodman <brad@bradgoodman.com>
2020-12-29 21:52:17 -0500

    Moar slack

--- slacktest.py
+++ slacktest.py
@@ -26,11 +26,29 @@ from authlibs.init import authbackend_init, createDefaultUsers
 import requests,urllib
 import logging, logging.handlers
 from  authlibs import eventtypes
+from slack_sdk.rtm import RTMClient
 
 Config = configparser.ConfigParser({})
 Config.read('makeit.ini')
-slack_api_token = Config.get('Slack','BOT_API_TOKEN')
+bot_api_token = Config.get('Slack','BOT_API_TOKEN')
 admin_api_token = Config.get('Slack','ADMIN_API_TOKEN')
+
+OPENED=False
+@RTMClient.run_on(event='open')
+def onopen(**payload):
+    print ("Opened",payload)
+    global OPENED
+    global rtmclient
+    OPENED=True
+    rtmclient.stop()
+
+@RTMClient.run_on(event='error')
+def onopen(**payload):
+    print ("ERROR",payload)
+    global OPENED
+    global rtmclient
+    OPENED=True
+    rtmclient.stop()
 
 if __name__ == '__main__':
         parser=argparse.ArgumentParser()
@@ -38,23 +56,24 @@ if __name__ == '__main__':
         (args,extras) = parser.parse_known_args(sys.argv[1:])
 
         print ("ADMIN API TOKEN",admin_api_token)
-        sc1 = SlackClient(slack_api_token)
+        bot = SlackClient(bot_api_token)
         #if sc1.rtm_connect():
         #  print "RTM connected on BOT"
         #else:
         #  print "RTM Connection Failed"
         #  sys.exit(1)
+        rtmclient = RTMClient(token=bot_api_token)
 
-        print ("API TOKEN",slack_api_token)
-        res = sc1.api_call(
+        print ("BOT API TOKEN",bot_api_token)
+        res = bot.api_call(
           api_method="chat.postMessage",
           json={
               "channel":"U03126YLY",
-              "text":"Admin Test Message"
+              "text":"Bot Test Message"
           }
         )
         sc = SlackClient(admin_api_token)
-        res = sc1.chat_postMessage(
+        res = bot.chat_postMessage(
           channel="U03126YLY",
           text="Admin Test Message2"
           )
@@ -62,7 +81,7 @@ if __name__ == '__main__':
           api_method="chat.postMessage",
           json={
               "channel":"U03126YLY",
-              "text":"Bot Test Message"
+              "text":"Admin Test Message"
             }
           )
         next_cursor=None
@@ -93,8 +112,21 @@ if __name__ == '__main__':
         )
         print (res)
 
-        # Okay if this fails
-        res = sc.conversations_invite(
-        channel="CTN7EK3A9",
-        users="U03126YLY"
-        )
+        try:
+            # Okay if this fails
+            res = sc.conversations_invite(
+            channel="CTN7EK3A9",
+            users="U03126YLY"
+            )
+        except BaseException as e:
+            print ("Invite failed - PROBABLY OKAY: ",e)
+
+
+        """ 
+        Now we will try the RTM connection that the bot uses
+        """
+
+        print ("Opening RTM")
+        rtmclient.start()
+        print ("RTM Stopped")
+         
