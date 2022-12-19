c56ade34a0578848ba0c9ddc6232d09c54d1675a
root <root@authtest2.lan.makeitlabs.com>
2022-12-19 16:27:49 +0000

    Install/test comments and fixes

--- test/accesstest.sh
+++ test/accesstest.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-cd ~/authbackend_bkg
 curl  ${LIVE_URL}/api/v1/resources/frontdoor/acl > ~/live_doorbot_v1.acl
 curl  @${LIVE_URL}/api/v0/resources/frontdoor/acl > ~/live_doorbot_v0.acl
-#curl  https://bkgtest:${BKGTEST_APIKEY}@${STAGING_URL}/api/v1/resources/frontdoor/acl > ~/staging_doorbot_v1.acl
-#curl  https://bkgtest:${BKGTEST_APIKEY}@${STAGING_URL}/api/v0/resources/frontdoor/acl > ~/staging_doorbot_v0.acl
-curl  http://bkgtest:${BKGTEST_APIKEY}@127.0.0.1:5000/api/v1/resources/frontdoor/acl > ~/local_doorbot_v1.acl
-curl  http://bkgtest:${BKGTEST_APIKEY}@127.0.0.1:5000/api/v0/resources/frontdoor/acl > ~/local_doorbot_v0.acl
+curl  ${STAGING_URL}/api/v1/resources/frontdoor/acl > ~/local_doorbot_v1.acl
+curl  @${STAGING_URL}/api/v0/resources/frontdoor/acl > ~/local_doorbot_v0.acl
 #echo "V1 DIFFS"
 #diff ~/live_doorbot_v1.acl ~/local_doorbot_v1.acl
 echo "V0 DIFFS"
